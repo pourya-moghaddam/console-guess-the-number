6eb8d867108b18a13b1e3b65b7afd10083048a33
isolium <mr.tester.2004@gmail.com>
2024-01-20 11:47:59 +0330

    Initial commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.Write("In what range of numbers do want to play? (1 to ?) ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int secretNumber = random.Next(1, max + 1);
+            int numberGuessed = 0;
+            Console.WriteLine("Random num: {0}", secretNumber);
+
+            do
+            {
+                Console.WriteLine("Enter a Number between 1 & {0}", max);
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+
+                if (numberGuessed < secretNumber)
+                    Console.WriteLine("It's higher!");
+                else if (numberGuessed > secretNumber)
+                    Console.WriteLine("It's lower!");
+
+            } while (numberGuessed != secretNumber);
+
+            Console.WriteLine("You guessed it. It was {0}!", secretNumber);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Guess the Number game in C#
+
+## A simple console Guess the Number game written in C#
+
+This project was built as a little practice of programming. It provides the feature of choosing the range that the number is picked from and telling the user if the number is higher or lower than their guess.
